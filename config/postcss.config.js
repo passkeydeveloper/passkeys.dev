ab1cf6e04195a6d1a01382cad4a0de406e467766
Tim Cappalli <tim@cloudauth.dev>
2023-12-29 19:11:05 +0000

    core config changes for v1

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -6,33 +6,51 @@ module.exports = {
   plugins: [
     autoprefixer(),
     purgecss({
-      content: [
-        './node_modules/@hyas/doks/layouts/**/*.html',
-        './node_modules/@hyas/doks/content/**/*.md',
-        './layouts/**/*.html',
-        './content/**/*.md',
+      content: [ './hugo_stats.json' ],
+      extractors: [
+        {
+          extractor: (content) => {
+            const els = JSON.parse(content).htmlElements;
+            return els.tags.concat(els.classes, els.ids);
+          },
+          extensions: ['json'],
+        },
+      ],
+      dynamicAttributes: [
+        'aria-expanded',
+        'data-bs-popper',
+        'data-bs-target',
+        'data-bs-theme',
+        'data-dark-mode',
+        'data-global-alert',
+        'data-pane',             // tabs.js
+        'data-popper-placement',
+        'data-sizes',
+        'data-toggle-tab',       // tabs.js
+        'id',
+        'size',
+        'type',
       ],
       safelist: [
+        'active',
+        'btn-clipboard',         // clipboards.js
+        'clipboard',             // clipboards.js
+        'disabled',
+        'hidden',
+        'modal-backdrop',        // search-modal.js
+        'selected',              // search-modal.js
+        'show',
+        'img-fluid',
+        'blur-up',
         'lazyloaded',
-        'table',
-        'thead',
-        'tbody',
-        'tr',
-        'th',
-        'td',
-        'h5',
-        'alert-link',
-        'container-xxl',
-        'container-fluid',
+        'container-fw ',
         ...whitelister([
-          './node_modules/@hyas/doks/assets/scss/common/_variables.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_alerts.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_buttons.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_code.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_syntax.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_search.scss',
-          './node_modules/@hyas/doks/assets/scss/common/_dark.scss',
-          './node_modules/katex/dist/katex.css',
+          './assets/scss/**/*.css',
+          './assets/scss/**/*.scss',
+          // './node_modules/katex/dist/katex.css',
+          './node_modules/@hyas/doks-core/assets/scss/components/_code.scss',
+          './node_modules/@hyas/doks-core/assets/scss/components/_expressive-code.scss',
+          './node_modules/@hyas/doks-core/assets/scss/common/_syntax.scss',
         ]),
       ],
     }),
