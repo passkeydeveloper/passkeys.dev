bfe839c8875bf1b0d16a7747b8e8cad9f5e8d8de
Tim Cappalli <tim@cloudauth.dev>
2026-04-08 10:13:37 -0400

    Move Hugo to build script

--- cf-build.sh
+++ cf-build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
+HUGO_VERSION="0.158.0"
 DART_SASS_VERSION="1.99.0"
+
+# Install Hugo Extended
+curl -sLO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+tar -xzf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+export PATH="$PATH:$PWD"
 
 # Install Dart Sass
 curl -sLO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
@@ -14,10 +20,7 @@ npm install
 # Fetch full git history (needed for Hugo's enableGitInfo)
 git fetch --unshallow
 
-if [ "$CF_PAGES_BRANCH" == "production" ]; then
-    hugo --gc --minify
-
-elif [ "$CF_PAGES_BRANCH" == "main" ]; then
+if [ "$CF_PAGES_BRANCH" == "main" ]; then
     hugo --gc --minify
 
 else
